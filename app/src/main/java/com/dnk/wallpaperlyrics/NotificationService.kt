191e5aa3783e2f95af0a51c4aa46ebb675acdf12
Daniel Hrehor <dan.hrehor@gmail.com>
2026-05-07 20:35:05 +0200

    Initial commit

---
+++ app/src/main/java/com/dnk/wallpaperlyrics/NotificationService.kt
@@ -0,0 +1,7 @@
+package com.dnk.wallpaperlyrics
+
+import android.service.notification.NotificationListenerService
+
+class NotificationService : NotificationListenerService() {
+    // We don't need to do anything here, just having it allows us to request notification access
+}
